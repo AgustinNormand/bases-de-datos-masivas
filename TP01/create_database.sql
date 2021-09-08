ed58f35c131d96f41b93930c55490eee6a17b8ac
Agustin Normand <NormandAgustin@gmail.com>
2021-09-08 20:48:06 -0300

    Add files from TP1

--- TP01/create_database.sql
+++ TP01/create_database.sql
@@ -6,105 +6,105 @@ CREATE DATABASE medios
     CONNECTION LIMIT = -1;
     
 /* Creo la tabla de Medios */
-CREATE TABLE IF NOT EXISTS public."Medios"
+CREATE TABLE IF NOT EXISTS public."medios"
 (
     id bigint NOT NULL,
     direccion character varying(50),
     nombre character varying(20),
-    CONSTRAINT "Medios_pkey" PRIMARY KEY (id)
+    CONSTRAINT "medios_pkey" PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Medios"
+ALTER TABLE public."medios"
     OWNER to postgres;
     
     
 /* Creo la tabla de Especialidades */
-CREATE TABLE IF NOT EXISTS public."Especialidades"
+CREATE TABLE IF NOT EXISTS public."especialidades"
 (
     id bigint NOT NULL,
     descripcion character varying(200),
-    CONSTRAINT "Especialidades_pkey" PRIMARY KEY (id)
+    CONSTRAINT "especialidades_pkey" PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Especialidades"
+ALTER TABLE public."especialidades"
     OWNER to postgres;
     
     
 /* Creo la tabla de Tipos_Medios */
-CREATE TABLE IF NOT EXISTS public."Tipos_Medio"
+CREATE TABLE IF NOT EXISTS public."tipos_medio"
 (
     id bigint NOT NULL,
     descripcion character varying(200),
-    CONSTRAINT "Tipos_Medio_pkey" PRIMARY KEY (id)
+    CONSTRAINT "tipos_medio_pkey" PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Tipos_Medio"
+ALTER TABLE public."tipos_medio"
     OWNER to postgres;
     
 
 /* Creo la tabla de Ciudades */
-CREATE TABLE IF NOT EXISTS public."Ciudades"
+CREATE TABLE IF NOT EXISTS public."ciudades"
 (
     id bigint NOT NULL,
     nombre character varying(20),
-    CONSTRAINT "Ciudades_pkey" PRIMARY KEY (id)
+    CONSTRAINT "ciudades_pkey" PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Ciudades"
+ALTER TABLE public."ciudades"
     OWNER to postgres;
 
 
 /* Creo la tabla de Provincias */
-CREATE TABLE IF NOT EXISTS public."Provincias"
+CREATE TABLE IF NOT EXISTS public."provincias"
 (
     id bigint NOT NULL,
     nombre character varying(20),
-    CONSTRAINT "Provincias_pkey" PRIMARY KEY (id)
+    CONSTRAINT "provincias_pkey" PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Provincias"
+ALTER TABLE public."provincias"
     OWNER to postgres;
     
 /* Agrego las claves foraneas a la tabla de Medios */
-ALTER TABLE public."Medios"
+ALTER TABLE public."medios"
     ADD COLUMN id_especialidad bigint,
     ADD CONSTRAINT id_especialidad_fk FOREIGN KEY (id_especialidad)
-    REFERENCES public."Especialidades" (id)
+    REFERENCES public."especialidades" (id)
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
     
-ALTER TABLE public."Medios"   
+ALTER TABLE public."medios"   
     ADD COLUMN id_tipo_medio bigint,
     ADD CONSTRAINT id_tipo_medio_fk FOREIGN KEY (id_tipo_medio)
-    REFERENCES public."Tipos_Medio" (id)
+    REFERENCES public."tipos_medio" (id)
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
     
-ALTER TABLE public."Medios"    
+ALTER TABLE public."medios"    
     ADD COLUMN id_ciudad bigint,
     ADD CONSTRAINT id_ciudad_fk FOREIGN KEY (id_ciudad)
-    REFERENCES public."Ciudades" (id)
+    REFERENCES public."ciudades" (id)
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;  
     
 /* Agrego las claves foraneas a la tabla de Ciudades */
-ALTER TABLE public."Ciudades"
+ALTER TABLE public."ciudades"
     ADD COLUMN id_provincia bigint,
     ADD CONSTRAINT id_provincia_fk FOREIGN KEY (id_provincia)
-    REFERENCES public."Provincias" (id)
+    REFERENCES public."provincias" (id)
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
@@ -112,23 +112,23 @@ ALTER TABLE public."Ciudades"
     
 /* Inserto datos de prueba */
 
-INSERT INTO public."Provincias"(
+INSERT INTO public."provincias"(
 	id, nombre)
 	VALUES (1, 'Buenos Aires');
 
-INSERT INTO public."Ciudades"(
+INSERT INTO public."ciudades"(
 	id, nombre, id_provincia)
 	VALUES (1, 'Luján', 1);
 	
-INSERT INTO public."Tipos_Medio"(
+INSERT INTO public."tipos_medio"(
 	id, descripcion)
 	VALUES (1, 'Probando');
 	
-INSERT INTO public."Especialidades"(
+INSERT INTO public."especialidades"(
 	id, descripcion)
 	VALUES (1, 'Probando');
 	
-INSERT INTO public."Medios"(
+INSERT INTO public."medios"(
 	id, nombre, id_especialidad, id_tipo_medio, direccion, id_ciudad)
 	VALUES (1, 
 	        'MedioProbando',
